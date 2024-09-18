db0312c09a968e1c4d1435d65ac6e33d5ceb9d91
Daria May <dariamechta@gmail.com>
2024-09-18 19:58:56 +0300

    added 01_04.c-02_01.c

---
+++ src/01_04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int left, right;
+
+    scanf("%d%d", &left, &right);
+    printf("%d", left * right);
+
+    return 0;
+}
---
+++ src/01_06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int value;
+    double pi = 3.141;
+
+    scanf("%d", &value);
+    printf("%.2lf", pi * value);
+
+    return 0;
+}
