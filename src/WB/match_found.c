51472d505bf90b8ea1f344e0b8d353f20c0bf682
DashulyaGor <dariamechta@gmail.com>
2025-06-05 23:26:22 +0300

    added testing feature for WB

---
+++ src/WB/match_found.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    char array[256];
+    int x, y;
+    scanf("%d%d", &x, &y);
+    if (y == x) {
+    // make a positive signal
+    } else {
+    // make a negative signal
+    }
+
+    input string if next string is equale
+    reading code
+    finding in the array the matchs
+    makes a signal
+    create a visual interface
+    pack it all in a container
+    connect these threads to execute the code
+    */
+}
